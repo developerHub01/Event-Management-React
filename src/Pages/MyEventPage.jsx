a90b264ec9fa03a7d748fbd8bbfa64d52504b440
developerHub01 <developerhubytchannel@gmail.com>
2023-10-09 14:48:33 +0600

    minor changes

--- src/Pages/MyEventPage.jsx
+++ src/Pages/MyEventPage.jsx
@@ -5,21 +5,23 @@ import useGetSetLocalStorage from "../CustomHook/useGetSetLocalStorage";
 const MyEventPage = () => {
   const { getEventList } = useGetSetLocalStorage();
   return (
-    <div
-      className={`w-[90%] max-w-6xl mx-auto min-h-screen py-5 place-items-center grid ${
-        getEventList("purchaseEventList") &&
-        "grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
-      } gap-5`}
-    >
-      {getEventList("purchaseEventList") ? (
-        getEventList("purchaseEventList").map((item) => (
-          <Service key={item.id} {...item} />
-        ))
-      ) : (
-        <h2 className="text-center text-3xl text-pink-700 font-bold">
-          No Event Added
-        </h2>
-      )}
+    <div className="flex justify-center items-center">
+      <div
+        className={`w-[90%] max-w-6xl mx-auto min-h-screen py-5 place-items-center grid ${
+          getEventList("purchaseEventList") &&
+          "grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+        } gap-5`}
+      >
+        {getEventList("purchaseEventList") ? (
+          getEventList("purchaseEventList").map((item) => (
+            <Service key={item.id} {...item} />
+          ))
+        ) : (
+          <h2 className="text-center text-3xl text-pink-700 font-bold">
+            No Event Added
+          </h2>
+        )}
+      </div>
     </div>
   );
 };
